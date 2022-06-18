8974ee22c06b41fc3419e7e5291917699119794d
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-06-18 16:20:56 -0300

    FEAT: created MovieDetail Model and ViewModel

--- MoviesApp/Services/HTTPClient.swift
+++ MoviesApp/Services/HTTPClient.swift
@@ -8,28 +8,46 @@
 import Foundation
 
 enum NetworkError: Error {
-    case badURL
-    case noData
-    case decodingError
+  case badURL
+  case noData
+  case decodingError
 }
 
 class HTTPClient {
-    func getMoviesBy(search: String, completion: @escaping (Result<[Movie]?, NetworkError>) -> Void) {
-        guard let url = URL.forMoviesByName(search) else {
-            return completion(.failure(.badURL))
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, res, error in
-            guard let data = data, error == nil else {
-                return completion(.failure(.noData))
-            }
-            
-            guard let moviesResponse = try? JSONDecoder().decode(MovieResponse.self, from: data) else {
-                return completion(.failure(.decodingError))
-            }
-            
-            completion(.success(moviesResponse.movies))
-            
-        }.resume()
+  func getMovieDetailsBy(imdbId: String, completion: @escaping (Result<MovieDetail, NetworkError>) -> Void) {
+    guard let url = URL.forMoviesByImdbId(imdbId) else {
+      return completion(.failure(.badURL))
     }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      guard let data = data, error == nil else {
+        return completion(.failure(.noData))
+      }
+      
+      guard let movieDetail = try? JSONDecoder().decode(MovieDetail.self, from: data) else {
+        return completion(.failure(.decodingError))
+      }
+      
+      completion(.success(movieDetail))
+    }.resume()
+  }
+  
+  func getMoviesBy(search: String, completion: @escaping (Result<[Movie]?, NetworkError>) -> Void) {
+    guard let url = URL.forMoviesByName(search) else {
+      return completion(.failure(.badURL))
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, res, error in
+      guard let data = data, error == nil else {
+        return completion(.failure(.noData))
+      }
+      
+      guard let moviesResponse = try? JSONDecoder().decode(MovieResponse.self, from: data) else {
+        return completion(.failure(.decodingError))
+      }
+      
+      completion(.success(moviesResponse.movies))
+      
+    }.resume()
+  }
 }
